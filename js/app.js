8c564dcaf09e864b165497d088b2b97b196c5f67
Corey McDaniel <corey.r.mcdaniel@live.com>
2015-09-18 16:27:50 -0700

    Updated controller for word fill

--- js/app.js
+++ js/app.js
@@ -2,11 +2,56 @@ angular.module('myApp', [])
     .constant('VERSION', 1.1)
     .controller('myCtrl', function (VERSION, $scope) {
         $scope.version = VERSION;
-    });
+    })
+    .controller('wordCtrl', function ($scope) {
+        var gender = "";
 
+        $scope.genderOptions = ["Male", "Female"];
+        $scope.updateGender = function (selGender) {
+            console.log(selGender);
+            if (selGender) {
+                gender = selGender;
+                $scope.noGenderError = false;
+            }
+        };
+        $scope.updateStory = function () {
+            if (!$scope.selectedGender) {
+                $scope.noGenderError = true;
+            } else {
+                $scope.noGenderError = false;
 
-$(document).ready(function () {
-    $("input").on("focus", function () {
-        $(this).select();
+                if ($scope.wordForm.$valid) {
+                    console.log('valid');
+
+                    if (gender == "Male") {
+                        $scope.his_her = "his";
+                        $scope.he_she = "he";
+                        $scope.him_her = "him";
+                    } else if (gender == "Female") {
+                        $scope.his_her = "her";
+                        $scope.he_she = "she";
+                        $scope.him_her = "her";
+                    } else {
+                        console.log("IDK what it is");
+                    }
+
+                    $scope.name = $scope.name_input;
+                    $scope.job_title = $scope.job_title_input;
+                    $scope.tedious_task = $scope.tedious_task_input;
+                    $scope.dirty_task = $scope.dirty_task_input;
+                    $scope.celebrity = $scope.celebrity_input;
+                    $scope.useless_skill = $scope.useless_skill_input;
+                    $scope.adjective = $scope.adjective_input;
+                    $scope.obnoxiuous_celebrity = $scope.obnoxiuous_celebrity_input;
+                    $scope.huge_number = $scope.huge_number_input;
+
+                    $scope.showStory = true;
+                }
+            }
+        };
+        $scope.resetForm = function () {
+            $scope.noGenderError = false;
+            $scope.selectedGender = "";
+            $scope.showStory = false;
+        };
     });
-});

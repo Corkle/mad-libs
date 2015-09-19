4ebdafe18efa4e91ac9e2e6a25a5ee200c4b691c
Corey McDaniel <corey.r.mcdaniel@live.com>
2015-09-18 22:36:54 -0700

    Updated view and form validation

--- js/app.js
+++ js/app.js
@@ -1,24 +1,20 @@
 angular.module('myApp', [])
-    .constant('VERSION', "1.0")
+    .constant('VERSION', "1.2")
     .controller('myCtrl', function (VERSION, $scope) {
         $scope.version = VERSION;
     })
     .controller('wordCtrl', function ($scope) {
         var gender = "";
+        $scope.showWordFill = true;
 
         $scope.genderOptions = ["Male", "Female"];
         $scope.updateGender = function (selGender) {
             if (selGender) {
                 gender = selGender;
-                $scope.noGenderError = false;
-            }
-            else {
-                $scope.noGenderError = true;
             }
         };
         $scope.updateStory = function () {
             if ($scope.wordForm.$valid) {
-                $scope.invalidFormError = false;
                 if (gender == "Male") {
                     $scope.his_her = "his";
                     $scope.he_she = "he";
@@ -42,16 +38,26 @@ angular.module('myApp', [])
                 $scope.huge_number = $scope.huge_number_input;
 
                 $scope.showStory = true;
-            } else {
-                $scope.invalidFormError = true;
-                $scope.updateGender($scope.selectedGender);
+                $scope.showWordFill = false;
             }
         };
         $scope.resetForm = function () {
+            $scope.wordForm.$setPristine();
             $scope.name_input = "";
-            $scope.noGenderError = false;
-            $scope.invalidFormError = false;
+            $scope.job_title_input = "";
+            $scope.tedious_task_input = "";
+            $scope.dirty_task_input = "";
+            $scope.celebrity_input = "";
+            $scope.useless_skill_input = "";
+            $scope.adjective_input = "";
+            $scope.obnoxiuous_celebrity_input = "";
+            $scope.huge_number_input = "";
             $scope.selectedGender = "";
             $scope.showStory = false;
+        };
+        $scope.startOver = function () {
+            $scope.resetForm();
+            $scope.showStory = false;
+            $scope.showWordFill = true;
         };
     });
